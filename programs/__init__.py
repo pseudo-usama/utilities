7f1065d1c23533dd8b253a3bba483be9fa320e5e
PSEUDO <Pseudo.Developers@gmail.ccom>
2021-05-29 09:43:15 +0500

    exit command added in programs

--- programs/__init__.py
+++ programs/__init__.py
@@ -3,7 +3,7 @@ from emoji import start as start_emojies
 
 
 PROGRAMS = (
-    (('emoji', 'e'), start_emojies),
+    (('emoji', 'em'), start_emojies),
 )
 
 
@@ -11,11 +11,15 @@ def start():
     while True:
         command = input('\nWhich program do you want to start : ')
 
+        if command == 'exit' or command == 'e':
+            break
+
         res = list(filter(lambda prog: command in prog[0], PROGRAMS))
         if len(res) == 0:
             print('Sorry! no such commands')
             continue
 
+        clear_screen()
         res[0][1]() # Calling requested program
         clear_screen()
 
